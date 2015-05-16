b4a578d3d5f0f21c893216d5e50325371b35003a
rhilenova <rhilenova@gmail.com>
2015-05-16 12:11:17 -0400

    Moved create from template button in line with other buttons.

--- src/main/java/modmuss50/HardCoreMapRest/GuiTweaker.java
+++ src/main/java/modmuss50/HardCoreMapRest/GuiTweaker.java
@@ -7,17 +7,52 @@ import net.minecraft.client.gui.GuiSelectWorld;
 import net.minecraft.client.resources.I18n;
 import net.minecraftforge.client.event.GuiScreenEvent;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GuiTweaker {
 
-	public static final int BUTTON_ID = 405;
+	public static int BUTTON_ID = 405;
 
 	@SubscribeEvent()
 	public void onGuiInit(GuiScreenEvent.InitGuiEvent.Post evt) {
 		if (evt.gui instanceof GuiSelectWorld) {
 			List<GuiButton> buttonList = evt.buttonList;
-			GuiButton button = new GuiButton(BUTTON_ID, 1, 1, 150, 20, I18n.format("gui.hardcoremapreset.create_button"));
+			ArrayList<Integer> buttonIDList = new ArrayList<Integer>();
+
+			// Width - 40 - (2 x 4) for spaces
+			int width = (evt.gui.width - 48) / 3;
+			int yPosition = 1;
+
+			boolean used = false;
+			for (GuiButton button : buttonList) {
+				buttonIDList.add(button.id);
+				// 3 - create
+				if (button.id == 3) {
+					button.width = width;
+					button.xPosition = (evt.gui.width / 2) - (width / 2);
+					yPosition = button.yPosition;
+				}
+				// 1 - select
+				else if (button.id == 1) {
+					button.width = width;
+					button.xPosition = (evt.gui.width / 2) - (width / 2) - 4 - width;
+				}
+				else if (button.id == BUTTON_ID) {
+					used = true;
+				}
+			}
+
+			if (used) {
+				while (used) {
+					BUTTON_ID += 1;
+					if (buttonIDList.indexOf(BUTTON_ID) < 0) {
+						used = false;
+					}
+				}
+			}
+			int xPosition = (evt.gui.width / 2) + (width / 2) + 4;
+			GuiButton button = new GuiButton(BUTTON_ID, xPosition, yPosition, width, 20, I18n.format("gui.hardcoremapreset.create_button"));
 			buttonList.add(button);
 		}
 	}
