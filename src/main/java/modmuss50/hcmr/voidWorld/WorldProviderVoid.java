bd13dd17b2f924da313591181a1dca1e33ac0622
modmuss50 <modmuss50@gmail.com>
2018-11-20 12:46:23 +0000

    Only change spawnpoint when in void world

--- src/main/java/modmuss50/hcmr/voidWorld/WorldProviderVoid.java
+++ src/main/java/modmuss50/hcmr/voidWorld/WorldProviderVoid.java
@@ -7,11 +7,17 @@ public class WorldProviderVoid extends WorldProviderSurface {
 
 	@Override
 	public BlockPos getRandomizedSpawnPoint() {
+		if(world.getWorldType() != HCMRWorldTypes.VOID_TYPE){
+			return super.getRandomizedSpawnPoint();
+		}
 		return new BlockPos(getSpawnPoint());
 	}
 
 	@Override
 	public BlockPos getSpawnPoint() {
+		if(world.getWorldType() != HCMRWorldTypes.VOID_TYPE){
+			return super.getSpawnPoint();
+		}
 		return new BlockPos(0, 96, 0);
 	}
 }
